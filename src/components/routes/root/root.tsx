3a16888c3d30607405fe65dc7b47bc9cf36dc0fb
Hernan Garzon <hernan.garzon@distillery.com>
2024-03-22 15:57:55 -0300

    Move search to root

--- src/components/routes/root/root.tsx
+++ src/components/routes/root/root.tsx
@@ -1,11 +1,10 @@
 import React, { FC } from 'react'
 
-import { Box } from '@mui/material'
+import { Box, TextField } from '@mui/material'
 import { usePodcasts } from '../../../api/podcast.api';
 import PodcastList from '../../PodcastList/podcastList';
 import { TPodcastList } from '../../../types/podcast.api';
 import PodcastHeader from '../../PodcastHeader/podcastHeader';
-import PodcastFilter from '../../PodcastFilter/podcastFilter';
 
 const Root: FC<{}> = () => {
   const { data: podcastData, isLoading, error } = usePodcasts();
@@ -23,7 +22,22 @@ const Root: FC<{}> = () => {
   return (
     <Box sx={{maxWidth: '800px', margin: '0 auto', padding: '30px'}}>
       <PodcastHeader isLoading={isLoading} podcasts={podcastData as TPodcastList} />
-      <PodcastFilter podcasts={podcastData as TPodcastList} />
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: '20px'
+        }}>
+        <Box>{podcastData?.length}</Box>
+        <TextField
+          id="outlined-basic"
+          label="Filter podcasts..."
+          variant="outlined"
+          sx={{
+            width: '300px',
+            marginLeft: '20px'
+          }}
+        />
+      </Box>
       <PodcastList isLoading={isLoading} podcasts={podcastData as TPodcastList} />
     </Box>
   )
